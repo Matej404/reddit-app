a39f1ba5c0a27af088f2b647b663f975d04a7532
Matej404 <matejjurin278@gmail.com>
2023-03-01 13:26:30 +0000

    home

---
+++ src/Features/Comment/Comment.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Comment.css";
+import Avatar from "../Avatar/Avatar";
+import moment from "moment";
+import ReactMarkdown from "react-markdown";
+
+export default function Comment(props) {
+    const { comment } = props;
+
+    return(
+        <div className="comment">
+        <div className="comment-metadata">
+          <Avatar name={comment.author} />
+          <p className="comment-author">{comment.author}</p>
+          <p className="comment-created-time">
+            {moment.unix(comment.created_utc).fromNow()}
+          </p>
+        </div>
+        <ReactMarkdown source={comment.body} />
+      </div>
+    )
+}
--- src/Features/Home/Home.js
+++ src/Features/Home/Home.js
@@ -1,17 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Home.css";
 import Post from "../Post/Post";
-import { useSelector } from "react-redux";
-import { selectedFilteredPosts } from "../../store/redditSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { selectFilteredPosts,
+        fetchPosts,
+        fetchComments,
+        setSearchTerm } from "../../store/redditSlice";
+import { AnimatedList } from "react-animated-list";
+import getRandomNumber from "../../utils/getRandomNumber";
+import PostLoading from "../Post/PostLoading";
 
 export default function Home() {
-    const posts = useSelector(selectedFilteredPosts);
+    const reddit = useSelector((state) => state.reddit);
+    const { searchTerm, isLoading, error, selectedSubreddit } = reddit;
+    const posts = useSelector(selectFilteredPosts);
+    const dispatch = useDispatch();
+    console.log(posts)
+
+    useEffect(() => {
+      dispatch(fetchPosts(selectedSubreddit));
+    }, [selectedSubreddit]);
+
+    const onToggleComments = (index) => {
+        const getComments = (permalink) => {
+            dispatch(fetchComments(index, permalink))
+        };
+
+        return getComments
+    }
+
+    if (isLoading) {
+      return (
+        <AnimatedList animation="zoom">
+          {Array(getRandomNumber(3, 10)).fill(<PostLoading />)}
+        </AnimatedList>
+      );
+    }
+
+    if (!error) {
+        return (
+          <div className="error">
+            <h2>Failed to load posts.</h2>
+            <button
+              type="button"
+              onClick={() => dispatch(fetchPosts(selectedSubreddit))}
+            >
+              Try again
+            </button>
+          </div>
+        );
+      }
+
+      if (posts.length === 0) {
+        return (
+          <div className="error">
+            <h2>No posts matching "{searchTerm}"</h2>
+            <button type="button" onClick={() => dispatch(setSearchTerm(''))}>
+              Go home
+            </button>
+          </div>
+        );
+      }
 
     return(
         <div>
-            {posts.map((post) => (
-                <Post post={post} key={post.id} />
-            ))}
+          {posts.map((post, index) => (
+            <Post
+                key={post.id}
+                post={post}
+                onToggleComments={onToggleComments(index)}
+                />
+              ))}
         </div>
     )
 }
