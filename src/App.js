b5d439f2f39dd55a41fb4242a1899c469d74cd8b
Matej404 <matejjurin278@gmail.com>
2023-03-06 09:57:39 +0000

    add Dubreddit component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./app.css";
 import Header from "./Features/Header/Header";
 import Home from "./Features/Home/Home";
-//import Subreddits from "./Features/Subreddits/Subreddits";
+import Subreddits from "./Features/Subreddits/Subreddits";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Home />
+      <aside>
+        <Subreddits />
+      </aside>
     </div>
   );
 }
